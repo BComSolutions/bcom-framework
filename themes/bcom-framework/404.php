38e4d840327abd75b38e940ddea3234df8c3b3ee
juliekuehl <julie@juliekuehl.com>
2016-02-15 09:38:49 -0600

    wrappers deleted and center-text added

--- themes/bcom-framework/404.php
+++ themes/bcom-framework/404.php
@@ -9,32 +9,26 @@ get_header(); ?>
 
 <section class="main" role="main">
 
-	<div class="container">
+	<section class="not-found">
 
-		<section class="not-found">
-			<div class="page-content">
+		<h2 class="four-oh-four-header">404</h2>
+		<p class="four-oh-four-subheader"><?php _e( "Oops! Something went wrong.", 'bcom' ); ?></p>
 
-				<h2 class="four-oh-four-header">404</h2>
-				<p class="four-oh-four-subheader"><?php _e( "Oops! Something went wrong.", 'bcom' ); ?></p>
+		<p class="four-oh-four-text"><?php _e( "You can head back to the Home Page.", 'bcom' ); ?></p>
+		<div class="center-text">
+			<a href="<?php bloginfo( 'url' ); ?>">
+				<div class="button-primary"><?php _e( "Back to Home Page", 'bcom' ); ?></div>
+			</a>
+		</div>
 
-				<p class="four-oh-four-text"><?php _e( "You can head back to the Home Page.", 'bcom' ); ?></p>
-				<div class="aligncenter center-text">
-					<a href="<?php bloginfo( 'url' ); ?>">
-						<div class="button-primary">
-							<?php _e( "Back to Home Page", 'bcom' ); ?>
-						</div>
-					</a>
-				</div>
+		<br/>
 
-				<br/>
+		<p class="four-oh-four-text"><?php _e( "Or maybe try a search?", 'bcom' ); ?></p>
+		<div class="four-oh-four-search">
+			<?php get_search_form(); ?>
+		</div>
 
-				<p class="four-oh-four-text"><?php _e( "Or maybe try a search?", 'bcom' ); ?></p>
-				<div class="four-oh-four-search">
-					<?php get_search_form(); ?>
-				</div>
-
-			</div><!-- .page-content -->
-		</section><!-- .not-found -->
+	</section><!-- .not-found -->
 
 </section><!-- #main -->
 
